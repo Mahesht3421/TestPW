b96174080c81506c0672d0a31cd964a3f9046419
MaheshTeja <mahesh.p.teja@gmail.com>
2026-05-21 21:26:47 +0530

    First Commit

--- PWMay2026/Typescript/github.ts
+++ PWMay2026/Typescript/github.ts
@@ -6,9 +6,15 @@
 //1.You are the first person Test Automation (There is no repo available)
         //0. Create new repository on the version control
         //1. Craete a project and write the test cases in the local systems
-        //2.git init - It initialize a new repo inside your local
-        //3.git add . - To add  all the files that you have chnaged/added in your local
+        //2. "git init" - It initialize a new repo inside your local
+        //3. "git add ."" - To add  all the files that you have chnaged/added in your local
         //   Local git area
-        //4.git commit -m "First Commit" - Commit all git
+        //4. "git commit -m "First Commit""" - Commit all changes to the local git
+        //5. "git branch -M master" - Create a new branch  named as master
+        //6. "git branch" - will list all the branches that are avilable in the repository
+        //7. git remote add origin https://github.com/Mahesht3421/TestPW.git --  Adds 
+        // a remote repo to your local repo and write the test cases inside your local system
+        //8. git push -u origin master =  push the commited changes from the local master branch 
+        //  to remote repository 
 
-//2.
+//2.If the repository is already available
